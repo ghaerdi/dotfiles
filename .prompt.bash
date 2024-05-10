0264a8c6ca97977dff6cdca35d73d567fff13176
ghaerdi <ghaerdi@outlook.com>
2024-05-10 18:53:52 -0400

    update

--- .prompt.bash
+++ .prompt.bash
@@ -1,2 +1,2 @@
-COMMANDS=("neofetch --pixterm ~/.config/neofetch/yae.jpg" "colorscript -e panes" "colorscript -e blocks1" "colorscript -e spectrum" "colorscript -e bars")
+COMMANDS=("uwufetch" "neofetch --pixterm ~/.config/neofetch/yae.jpg" "colorscript -e panes" "colorscript -e blocks1" "colorscript -e spectrum" "colorscript -e bars")
 $(shuf -n1 -e "${COMMANDS[@]}")
