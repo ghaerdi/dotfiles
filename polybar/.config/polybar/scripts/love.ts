ff4e40f2c1540639431d66b6479e4e99144d1eb3
ghaerdi <ghaerdi@outlook.com>
2024-06-20 03:50:17 -0400

    feat(polybar): add love script

---
+++ polybar/.config/polybar/scripts/love.ts
@@ -0,0 +1,15 @@
+#!/usr/bin/env bun
+
+const phrases = [
+  "Cause I think I've fallen in love this time",
+  "I blinked and suddenly had a valentine",
+  "We are destined to love each other",
+  "But life had to prepare us to be together first",
+  "Para mí tú serás único en el mundo",
+  "Y para ti yo seré único en el mundo"
+];
+
+const shuffle = <T>(array: T[]) => array.toSorted(() => Math.random() - 0.5);
+
+console.log(shuffle(phrases).at(0));
+
