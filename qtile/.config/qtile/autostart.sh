0e5cbd7f11f47aaecbf812220cda0c9a67766f8d
ghaerdi <ghaerdi@outlook.com>
2024-06-24 04:00:42 -0400

    feat(qtile): autostart obs

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -4,6 +4,7 @@
 picom -b &
 greenclip daemon &
 nm-applet &
-dunst & -config ~/.config/dunst/dunstrc &
+dunst -config ~/.config/dunst/dunstrc &
 xmodmap -e 'pointer = 3 2 1' &
-feh --bg-fill ~/.dotfiles/Wallpapers/pixel-art-wallpaper-20072220050556.png
+feh --bg-fill ~/.dotfiles/Wallpapers/pixel-art-wallpaper-20072220050556.png &
+obs --minimize-to-tray
