d81665446c9e8d062922d3ccd91432d146d59978
ghaerdi <ghaerdi@outlook.com>
2024-06-27 08:57:47 -0400

    feat(qtile): add blueman-applet and xinput settings to autostart

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -4,7 +4,8 @@
 picom -b &
 greenclip daemon &
 nm-applet &
+blueman-applet &
 dunst -config ~/.config/dunst/dunstrc &
 xmodmap -e 'pointer = 3 2 1' &
-feh --bg-fill ~/.dotfiles/Wallpapers/pixel-art-wallpaper-20072220050556.png &
-obs --minimize-to-tray
+xinput set-button-map "ASUF1204:00 2808:0104 Touchpad" 3 2 1 4 5 6 7 &
+feh --bg-fill ~/dotfiles/Wallpapers/pixel-art-wallpaper-20072220050556.png &
