1960e09c5f688f23469e7223c484b13ea7b52946
ghaerdi <ghaerdi@outlook.com>
2024-06-28 21:04:12 -0400

    feat(qtile): add ScratchPad and DropDown

--- qtile/.config/qtile/config.py
+++ qtile/.config/qtile/config.py
@@ -24,7 +24,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from libqtile.config import Click, Drag, Group, Key, Match, Screen
+from libqtile.config import Click, Drag, Group, Key, Match, Screen, ScratchPad, DropDown
 from libqtile.utils import guess_terminal
 from libqtile import layout, qtile, hook
 from libqtile.lazy import lazy
@@ -69,14 +69,18 @@ keys = [
     Key([mod], "space", lazy.layout.next(), desc="Move focus to other window"),
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key([mod, "shift"], "h", lazy.layout.shuffle_left(),
+        desc="Move window to the left"),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_right(),
+        desc="Move window to the right"),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
-    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
+    Key([mod, "control"], "h", lazy.layout.grow_left(),
+        desc="Grow window to the left"),
+    Key([mod, "control"], "l", lazy.layout.grow_right(),
+        desc="Grow window to the right"),
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
@@ -94,19 +98,24 @@ keys = [
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
     Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
-    Key([], "F11", lazy.window.toggle_fullscreen(), desc="Toggle fullscreen on the focused window",),
-    Key([mod], "f", lazy.window.toggle_floating(), desc="Toggle floating on the focused window"),
-    Key([mod, "control"], "r", lazy.reload_config(), lazy.spawn(Apps.polybar), desc="Reload the config"),
+    Key([], "F11", lazy.window.toggle_fullscreen(),
+        desc="Toggle fullscreen on the focused window",),
+    Key([mod], "f", lazy.window.toggle_floating(),
+        desc="Toggle floating on the focused window"),
+    Key([mod, "control"], "r", lazy.reload_config(),
+        lazy.spawn(Apps.polybar), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
     Key([mod], "Return", lazy.spawn(Apps.terminal), desc="Launch terminal"),
-    Key([mod], "space", lazy.spawn(Apps.launcher), desc="Search & Launch an app"),
+    Key([mod], "space", lazy.spawn(Apps.launcher),
+        desc="Search & Launch an app"),
     Key([mod], "period", lazy.spawn(Apps.emoji), desc="Search & pick an emoji"),
     Key([mod], "p", lazy.spawn(Apps.power_menu), desc="Open powermenu"),
     Key([], "XF86MonBrightnessUp", lazy.spawn("brightnessctl set +10%")),
     Key([], "XF86MonBrightnessDown", lazy.spawn("brightnessctl set 10%-")),
     Key([], "XF86AudioLowerVolume", lazy.spawn("pactl set-sink-volume 0 -5%")),
     Key([], "XF86AudioRaiseVolume", lazy.spawn("pactl set-sink-volume 0 +5%")),
-    Key([], "XF86AudioMute", lazy.spawn("pactl set-sink-mute @DEFAULT_SINK@ toggle")),
+    Key([], "XF86AudioMute", lazy.spawn(
+        "pactl set-sink-mute @DEFAULT_SINK@ toggle")),
     # Redshift
     Key([mod], "r", lazy.spawn("redshift -O 4000")),
     Key([mod, "shift"], "r", lazy.spawn("redshift -x")),
@@ -122,13 +131,14 @@ for vt in range(1, 8):
         Key(
             ["control", "mod1"],
             f"f{vt}",
-            lazy.core.change_vt(vt).when(func=lambda: qtile.core.name == "wayland"),
+            lazy.core.change_vt(vt).when(
+                func=lambda: qtile.core.name == "wayland"),
             desc=f"Switch to VT{vt}",
         )
     )
 
-
-groups = [Group(i) for i in ["I", "II", "III", "IV", "V", "VI", "VII", "VII", "IX"]]
+groups = [Group(i)
+          for i in ["I", "II", "III", "IV", "V", "VI", "VII", "VII", "IX"]]
 
 for i, group in enumerate(groups):
     actual_key = str(i+1)
@@ -146,7 +156,8 @@ for i, group in enumerate(groups):
                 [mod, "shift"],
                 actual_key,
                 lazy.window.togroup(group.name, switch_group=True),
-                desc="Switch to & move focused window to group {}".format(group.name),
+                desc="Switch to & move focused window to group {}".format(
+                    group.name),
             ),
             # Or, use below if you prefer not to switch to that group.
             # # mod + shift + group number = move focused window to group
@@ -155,6 +166,18 @@ for i, group in enumerate(groups):
         ]
     )
 
+groups.append(ScratchPad("scratchpad", [
+    DropDown("spotify", "spotify-launcher", x=0.05,
+             y=0.05, width=0.9, height=0.9, opacity=0.9,),
+]))
+
+keys.extend([
+    Key([mod], "y", lazy.group["scratchpad"].dropdown_toggle("spotify")),
+    Key([mod], "u", lazy.group["scratchpad"].dropdown_toggle("spotify")),
+    Key([mod], "i", lazy.group["scratchpad"].dropdown_toggle("spotify")),
+    Key([mod], "o", lazy.group["scratchpad"].dropdown_toggle("spotify")),
+])
+
 
 class Colors:
     primary = "#d3869b"
@@ -162,19 +185,9 @@ class Colors:
 
 
 layouts = [
-    layout.Columns(border_focus=Colors.primary, border_normal=Colors.secondary, margin=8, border_width=2),
-    layout.Max(margin=4),
-    # Try more layouts by unleashing below layouts.
-    # layout.Stack(num_stacks=2),
-    # layout.Bsp(),
-    # layout.Matrix(),
-    # layout.MonadTall(),
-    # layout.MonadWide(),
-    # layout.RatioTile(),
-    # layout.Tile(),
-    # layout.TreeTab(),
-    # layout.VerticalTile(),
-    # layout.Zoomy(),
+    layout.Columns(border_focus=Colors.primary,
+                   border_normal=Colors.secondary, margin=8, border_width=2),
+    layout.Max(margin=8),
 ]
 
 # SCREENS
@@ -183,8 +196,10 @@ screens = [Screen()]
 
 # Drag floating layouts.
 mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+    Drag([mod], "Button1", lazy.window.set_position_floating(),
+         start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(),
+         start=lazy.window.get_size()),
     Click([mod], "Button2", lazy.window.bring_to_front()),
 ]
 
