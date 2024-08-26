1433db96bfe8db9fa1187bd8cf99b0947f314b40
ghaerdi <ghaerdi@outlook.com>
2024-08-26 13:47:39 -0400

    style(qtile): update wallpaper path in autostart script

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -8,4 +8,4 @@ blueman-applet &
 dunst -config ~/.config/dunst/dunstrc &
 xmodmap -e 'pointer = 3 2 1' &
 xinput set-button-map "ASUF1204:00 2808:0104 Touchpad" 3 2 1 4 5 6 7 &
-feh --bg-fill ~/Wallpapers/pixel-art-wallpaper-20072220050556.png &
+feh --bg-fill ~/dotfiles/wallpapers/Wallpapers/gruvbox_astro.jpg &
