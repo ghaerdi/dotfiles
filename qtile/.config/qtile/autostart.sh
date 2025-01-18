887c8b9f974e4b314ef4f83fcb3682e7c9b96846
ghaerdi <ghaerdi@outlook.com>
2025-01-18 01:35:37 -0400

    style(qtile): change wallpapaer

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -10,4 +10,4 @@ dunst -config ~/.config/dunst/dunstrc &
 xmodmap -e 'pointer = 3 2 1' &
 xinput set-button-map "ASUF1204:00 2808:0104 Touchpad" 3 2 1 4 5 6 7 &
 xinput set-button-map "ASCP1A01:00 093A:3014 Touchpad" 3 2 1 4 5 6 7 &
-feh --bg-fill ~/dotfiles/wallpapers/Wallpapers/gruvbox_astro.jpg &
+feh --bg-fill ~/dotfiles/wallpapers/Wallpapers/pixel-art-wallpaper-20072220050556.png
