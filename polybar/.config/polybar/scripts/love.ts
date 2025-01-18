781d70994efc375929f03666c0b34a6297b8428f
ghaerdi <ghaerdi@outlook.com>
2025-01-18 01:36:05 -0400

    style(polybar): update colors and formatting

--- polybar/.config/polybar/scripts/love.ts
+++ polybar/.config/polybar/scripts/love.ts
@@ -6,7 +6,9 @@ const phrases = [
   "We are destined to love each other",
   "But life had to prepare us to be together first",
   "Para mí tú serás único en el mundo",
-  "Y para ti yo seré único en el mundo"
+  "Y para ti yo seré único en el mundo",
+  "Don't let me go",
+  "Remember me"
 ];
 
 const shuffle = <T>(array: T[]) => array.toSorted(() => Math.random() - 0.5);
