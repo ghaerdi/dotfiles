d8266e3d58fad1fd1b0ae2e67832275ca0a4538f
ghaerdi <ghaerdi@outlook.com>
2025-03-14 00:36:05 -0300

    nixconf: change xdg-desktop-portal to wlr

--- nixconf/hosts/laptop/configuration.nix
+++ nixconf/hosts/laptop/configuration.nix
@@ -6,11 +6,13 @@
 }: {
   imports = [
     ../../nixos/configuration.nix
-    ../../nixos/features/battery.nix
+    ../../nixos/features/cachix.nix
     ../../nixos/features/nvidia.nix
-    ../../nixos/features/steam.nix
+    ../../nixos/features/battery.nix
     ../../nixos/features/hyprland.nix
     ../../nixos/features/pipewire.nix
+    ../../nixos/features/steam.nix
+    ../../nixos/features/gnome.nix
   ];
   nixpkgs.config.allowUnfreePredicate = pkg:
     builtins.elem (lib.getName pkg) [
--- nixconf/nixos/features/hyprland.nix
+++ nixconf/nixos/features/hyprland.nix
@@ -3,17 +3,11 @@
   pkgs,
   ...
 }: {
-  nix.settings = {
-    substituters = ["https://hyprland.cachix.org"];
-    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
-  };
-
-  programs = {
-    hyprland = {
-      enable = true;
-      package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-      portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-      xwayland.enable = true;
-    };
+  programs.hyprland = {
+    enable = true;
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    # portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland; # broken
+    portalPackage = pkgs.xdg-desktop-portal-wlr;
+    xwayland.enable = true;
   };
 }
