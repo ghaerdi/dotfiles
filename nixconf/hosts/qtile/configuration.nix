a1cb0e72eb9b437146b73fa7bdf4159ae06959eb
ghaerdi <ghaerdi@outlook.com>
2025-07-27 16:35:08 -0400

    chore(nixconf): remove comments from allowUnfreePredicate

--- nixconf/hosts/qtile/configuration.nix
+++ nixconf/hosts/qtile/configuration.nix
@@ -18,11 +18,9 @@
   };
   nixpkgs.config.allowUnfreePredicate = pkg:
     builtins.elem (lib.getName pkg) [
-      # nvidia
       "nvidia-persistenced"
       "nvidia-settings"
       "nvidia-x11"
-      # steam
       "steam"
       "steam-original"
       "steam-unwrapped"
