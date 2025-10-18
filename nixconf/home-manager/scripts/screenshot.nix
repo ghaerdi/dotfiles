5b737306e24b188e280946765b214254ddf2cac5
ghaerdi <ghaerdi@outlook.com>
2025-10-17 22:03:09 -0300

    chore(nixconf): format

--- nixconf/home-manager/scripts/screenshot.nix
+++ nixconf/home-manager/scripts/screenshot.nix
@@ -5,22 +5,22 @@
 }: {
   home.packages = with pkgs; [
     (pkgs.writeShellScriptBin "vanzuh-screenshot" ''
-			#!/bin/bash
+      #!/bin/bash
 
-			[[ -f ~/.config/user-dirs.dirs ]] && source ~/.config/user-dirs.dirs
-			OUTPUT_DIR="$HOME/Pictures/screenshots"
+      [[ -f ~/.config/user-dirs.dirs ]] && source ~/.config/user-dirs.dirs
+      OUTPUT_DIR="$HOME/Pictures/screenshots"
 
-			if [[ ! -d "$OUTPUT_DIR" ]]; then
-				${pkgs.coreutils}/bin/mkdir -p "$OUTPUT_DIR"
-			fi
+      if [[ ! -d "$OUTPUT_DIR" ]]; then
+      	${pkgs.coreutils}/bin/mkdir -p "$OUTPUT_DIR"
+      fi
 
-			${pkgs.procps}/bin/pkill slurp || ${pkgs.hyprshot}/bin/hyprshot -m ''${1:-region} --raw |
-				${pkgs.satty}/bin/satty --filename - \
-					--output-filename "$OUTPUT_DIR/screenshot-$(${pkgs.coreutils}/bin/date +'%Y-%m-%d_%H-%M-%S').png" \
-					--early-exit \
-					--actions-on-enter save-to-clipboard \
-					--save-after-copy \
-					--copy-command '${pkgs.wl-clipboard}/bin/wl-copy'
+      ${pkgs.procps}/bin/pkill slurp || ${pkgs.hyprshot}/bin/hyprshot -m ''${1:-region} --raw |
+      	${pkgs.satty}/bin/satty --filename - \
+      		--output-filename "$OUTPUT_DIR/screenshot-$(${pkgs.coreutils}/bin/date +'%Y-%m-%d_%H-%M-%S').png" \
+      		--early-exit \
+      		--actions-on-enter save-to-clipboard \
+      		--save-after-copy \
+      		--copy-command '${pkgs.wl-clipboard}/bin/wl-copy'
     '')
   ];
 }
