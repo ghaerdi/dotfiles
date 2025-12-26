5fd369ae025ab65f4b0c2b602f4702a80e600802
ghaerdi <ghaerdi@outlook.com>
2025-12-25 20:44:42 -0400

    feat(nixconf): change system timezone

--- nixconf/nixos/configuration.nix
+++ nixconf/nixos/configuration.nix
@@ -80,8 +80,7 @@
     };
   };
 
-  # time.timeZone = "America/Santo_Domingo";
-  time.timeZone = "America/Santiago";
+  time.timeZone = "America/Santo_Domingo";
 
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.supportedLocales = ["en_US.UTF-8/UTF-8" "es_DO.UTF-8/UTF-8"];
