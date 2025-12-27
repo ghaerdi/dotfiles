f9bf3e2c6cf8d222ec6de0ee88f0b275c44681d1
ghaerdi <ghaerdi@outlook.com>
2025-12-26 23:27:18 -0400

    feat(nixconf): add isoimage configuration

---
+++ nixconf/hosts/isoimage/configuration.nix
@@ -0,0 +1,28 @@
+{
+  pkgs,
+  modulesPath,
+  inputs,
+  ...
+}: {
+  imports = [
+    "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
+  ];
+
+  nixpkgs.hostPlatform = "x86_64-linux";
+
+  environment.systemPackages = with pkgs; [
+    udiskie
+    neovim
+    git
+  ];
+
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.11"; # Did you read the comment?
+}
