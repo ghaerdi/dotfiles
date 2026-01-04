a500c757f35022a17477212f4c4c507617c04bc5
ghaerdi <ghaerdi@outlook.com>
2026-01-04 00:19:15 -0400

    feat(nixconf): update hyprland window pop script

--- nixconf/home-manager/scripts/hyprland-window-pop.nix
+++ nixconf/home-manager/scripts/hyprland-window-pop.nix
@@ -9,23 +9,47 @@
 
       # Toggle to pop-out a tile to stay fixed on a display basis.
 
-      active=$(${pkgs.hyprland}/bin/hyprctl activewindow -j)
-      pinned=$(echo "$active" | ${pkgs.jq}/bin/jq .pinned)
-      addr=$(echo "$active" | ${pkgs.jq}/bin/jq -r  ".address")
-      [ -z "$addr" ] && { echo "No active window"; exit 0; }
+      # Usage:
+      # nixconf-hyprland-window-pop [width height [x y]]
+      #
+      # Arguments:
+      #   width   Optional. Width of the floating window. Default: 1300
+      #   height  Optional. Height of the floating window. Default: 900
+      #   x       Optional. X position of the window. Must provide both X and Y to take effect.
+      #   y       Optional. Y position of the window. Must provide both X and Y to take effect.
+      #
+      # Behavior:
+      #   - If the window is already pinned, it will be unpinned and removed from the pop layer.
+      #   - If the window is not pinned, it will be floated, resized, moved/centered, pinned, brought to top, and popped.
 
-      if [ "$pinned" = "true" ]; then
+      width=''${1:-1300}
+      height=''${2:-900}
+      x=''${3:-}
+      y=''${4:-}
+
+      active=$(${pkgs.hyprland}/bin/hyprctl activewindow -j)
+      pinned=$(echo "$active" | ${pkgs.jq}/bin/jq ".pinned")
+      addr=$(echo "$active" | ${pkgs.jq}/bin/jq -r ".address")
+
+      if [[ $pinned == "true" ]]; then
         ${pkgs.hyprland}/bin/hyprctl -q --batch \
-        "dispatch pin address:$addr;" \
-        "dispatch togglefloating address:$addr;" \
-        "dispatch tagwindow -pop address:$addr;"
-      else
+          "dispatch pin address:$addr;" \
+          "dispatch togglefloating address:$addr;" \
+          "dispatch tagwindow -pop address:$addr;"
+      elif [[ -n $addr ]]; then
+        ${pkgs.hyprland}/bin/hyprctl dispatch togglefloating address:$addr
+        ${pkgs.hyprland}/bin/hyprctl dispatch resizeactive exact $width $height address:$addr
+
+        if [[ -n $x && -n $y ]]; then
+          ${pkgs.hyprland}/bin/hyprctl dispatch moveactive $x $y address:$addr
+        else
+          ${pkgs.hyprland}/bin/hyprctl dispatch centerwindow address:$addr
+        fi
+
         ${pkgs.hyprland}/bin/hyprctl -q --batch \
-        "dispatch togglefloating address:$addr;" \
-        "dispatch centerwindow address:$addr;" \
-        "dispatch pin address:$addr;" \
-        "dispatch alterzorder top address:$addr;" \
-        "dispatch tagwindow +pop address:$addr;"
+          "dispatch pin address:$addr;" \
+          "dispatch alterzorder top address:$addr;" \
+          "dispatch tagwindow +pop address:$addr;"
       fi
     '')
   ];
